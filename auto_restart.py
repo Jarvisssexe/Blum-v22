a953523375c98219b471bb41bdb5d8901be810fd
Jarvis <160870720+Jarvisssexe@users.noreply.github.com>
2025-04-08 19:38:03 +0530

    auto_restart.py

--- auto_restart.py
+++ auto_restart.py
@@ -1,15 +1,18 @@
 import time
 import os
 
+
 # Define the name of the main script
 MAIN_SCRIPT = 'main.py'
 
-def run_script():
-    while True:
-        print("Starting main.py...")
-        os.system(f'python {MAIN_SCRIPT}')  # Use os.system to run the script
-        print("main.py has finished. Restarting in 2 minutes...")
-        time.sleep(120)  # Sleep for 2 minutes (120 seconds)
 
-if name == "main":  # Use the correct condition
-    run_script()
+def run_script():
+    while True:
+        print("Starting main.py...")
+        os.system(f'python {MAIN_SCRIPT}') # Use os.system to run the script
+        print("main.py has finished. Restarting in 2 minutes...")
+        time.sleep(120) # Sleep for 2 minutes (120 seconds)
+
+
+if __name__ == "__main__": # Use the correct condition
+    run_script()
